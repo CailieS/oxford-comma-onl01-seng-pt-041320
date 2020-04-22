d5c45bb64392db8d60cd240ac8f71ab0271e9b03
Cailie Skelton <cskelton707@gmail.com>
2020-04-22 18:05:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
-  if array.amount == 2
+  if array.amount == 1
     array.join ("and")
+    elsif array.amount == 2
   elsif array.amount >= 3
     array[-1]  = "and" << array [-1]
     elsif array.join (", ")
