d1c3f4734674563b87e0cdecd5c01e15b083617e
Cailie Skelton <cskelton707@gmail.com>
2020-04-22 17:19:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,3 +8,15 @@ def oxford_comma(array)
     elsif [-1] "and" << array [-1]
     array.join (",")
 end
+
+def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
+end
