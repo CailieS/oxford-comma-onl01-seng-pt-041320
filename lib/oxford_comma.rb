a54d3321bc3d31b334739749c0fcb247912d0cf0
Cailie Skelton <cskelton707@gmail.com>
2020-04-22 19:05:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,14 +4,12 @@ require "pry"
 def oxford_comma(array)
   if array.length == 1
     array.join
-    
   elsif array.length == 2
-
-  return "#{array[0]} and #{array[1]}"
-
+    return "#{array[0]} and #{array[1]}"
   elsif array.length > 2
-  array[-1].insert(0, "and")
-  binding.pry
+    array[-1].insert(0, "and ")
+    return array.join(", ")
+ 
  
 end
 
